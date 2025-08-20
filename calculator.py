fc6c8abcdfffeb826087e4ea750f79cdf010de46
Maksim Cherniavskiy <stariytanky1993@gmail.com>
2025-08-20 15:40:14 +0300

    1.1

--- calculator.py
+++ calculator.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 
 from kivy.core.window import Window
 
@@ -10,23 +11,20 @@ Window.clearcolor = (100/255, 50/255, 200/255, 1)
 Window.title = ('Calculator')
 
 class MyApp(App):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.t = 1
-        self.label = Label(text=str(self.t))
-
-
-    def button_pressed(self, instance):
-        self.t += 1
-        self.label.text = str(self.t)
+    def num():
+        
         
     def build(self):
-        box = BoxLayout()
-        button = Button(text = "+")
-        button.bind(on_press = self.button_pressed)
-        box.add_widget(self.label)
-        box.add_widget(button)
-        return box
+        calc = BoxLayout(orientation = 'vertical')
+        board = Label(text = '')
+        calc.add_widget(board)
+        keypad = GridLayout(rows = 3, cols = 3)
+        for i in range(1, 10):
+            btn = Button(text = f"{i}")
+            keypad.add_widget(btn)
+        calc.add_widget(keypad)
+        return calc
+        
         
 
 if __name__ == '__main__':
