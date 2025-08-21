2061483b6b7e3e2e7be7ac95a647be9f22b51832
Maksim Cherniavskiy <stariytanky1993@gmail.com>
2025-08-21 10:19:51 +0300

    1.4

--- calculator.py
+++ calculator.py
@@ -15,6 +15,21 @@ class MyApp(App):
         self.board = Label(text = '')
         self.keypad = GridLayout(rows = 3, cols = 3)
         self.num_list = []
+        def equal_press(instance):
+            self.board.text = str(eval(self.board.text))
+        def signs_press(instance):
+            if self.board.text and self.board.text[-1] not in ['+', '-', '=']:
+                self.board.text += instance.text
+        plas = Button(text = '+')
+        plas.bind(on_press = signs_press)
+        self.signs = BoxLayout(orientation = 'vertical')
+        self.signs.add_widget(plas)
+        minus = Button(text = '-')
+        minus.bind(on_press = signs_press)
+        self.signs.add_widget(minus)
+        equal = Button(text = '=')
+        equal.bind(on_press = equal_press)
+        self.signs.add_widget(equal)
         def press(instance):
             self.board.text += instance.text
         for i in range(1, 10):
@@ -24,14 +39,18 @@ class MyApp(App):
         for btn in self.num_list:
             self.keypad.add_widget(btn)
 
-
+        
         
     def build(self):
         calc = BoxLayout(orientation = 'vertical')  
+        left = BoxLayout(orientation = 'horizontal')
         calc.add_widget(self.board)
+        left.add_widget(calc)
+        left.add_widget(self.signs)
         
         calc.add_widget(self.keypad)
-        return calc
+        return left
+        
         
         
 
